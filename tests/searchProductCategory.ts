a8c7a3398479f5ec833ae0fb5659385745e52bd3
Serval <ficelogan@gmail.com>
2024-10-21 16:45:10 +0200

    3.0 SearchProductCategory

--- tests/searchProductCategory.ts
+++ tests/searchProductCategory.ts
@@ -2,25 +2,24 @@ import { Page, Locator, expect } from "@playwright/test";
 
 export class SearchPage {
     private page: Page;
-    private categoryButton: Locator;
-    private automotiveCategoryButton: Locator;
-    private searchButton: Locator;
+    private burgerMenuButton: Locator;
+    private booksCategoryButton: Locator;
+    private allBooksButton: Locator;
     private searchResults: Locator;
 
     constructor(page: Page) {
         this.page = page;
-        this.categoryButton = this.page.locator('#nav-hamburger-menu'); // Sélecteur pour le bouton de catégorie
-        this.automotiveCategoryButton = this.page.locator('a[data-menu-id="9"]'); //SElecteur pour aller dans Livres
-        this.searchButton = this.page.locator('input[type="submit"]'); // Sélecteur pour le bouton de recherche
-        this.searchButton = this.page.locator('a[href="/gp/browse.html?node=301061&amp;ref_=nav_em__lv_0_2_9_2"]'); // Sélecteur pour le bouton de recherche
+        this.burgerMenuButton = this.page.locator('#nav-hamburger-menu'); // Sélecteur pour le menu burger
+        this.booksCategoryButton = this.page.locator('a[data-menu-id="9"]'); // Sélecteur pour "Livres"
+        this.allBooksButton = this.page.locator('a[href="/gp/browse.html?node=301061&ref_=nav_em__lv_0_2_9_2"]'); // Sélecteur pour "Tous les livres"
         this.searchResults = this.page.locator('.s-main-slot'); // Sélecteur pour les résultats de recherche
     }
 
-    async searchInCategory() {
-        await this.page.goto('https://www.amazon.com'); // Assure que tu es sur la page d'accueil
-        await this.categoryButton.click();
-        await this.automotiveCategoryButton.click();
-        await this.searchButton.click();
+    async searchInAllBooks() {
+        await this.page.goto('https://www.amazon.com'); // Aller sur la page d'accueil
+        await this.burgerMenuButton.click(); // Ouvre le menu burger
+        await this.booksCategoryButton.click(); // Clique sur "Livres"
+        await this.allBooksButton.click(); // Sélectionne "Tous les livres"
         
         // Vérification que les résultats sont affichés
         await expect(this.searchResults).toBeVisible();
