f752cb325be4000f38deeca8b5859742a60692ed
Romain <romain.pietri@student.junia.com>
2024-10-21 16:43:06 +0200

    Mybad

--- tests/addToCart.ts
+++ tests/addToCart.ts
@@ -7,8 +7,8 @@ export class AddToCartButton {
 
   constructor(page: Page) {
     this.page = page;
-    this.SelectionArticle = this.page.locator('.');
-    this.addToCartButton= this.page.locator('tesxt="ZKGYUS Tablier de cuisine Sexy Funny Novelty"')
+    this.SelectionArticle = this.page.locator('tesxt="ZKGYUS Tablier de cuisine Sexy Funny Novelty"');
+    this.addToCartButton= this.page.locator('#add-to-cart-button')
   }
 
   async gotoPageArticle(){
