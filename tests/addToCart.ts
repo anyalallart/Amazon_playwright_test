64d489f16ce75d0037ec96055d6c84ba71472aca
Romain <romain.pietri@student.junia.com>
2024-10-21 15:48:31 +0200

    AddtoCart Modif

--- tests/addToCart.ts
+++ tests/addToCart.ts
@@ -2,13 +2,18 @@ import { Page, Locator, expect } from "@playwright/test";
 
 export class AddToCartButton {
   private page: Page;
-  private addToCartButton : Locator;
+  private SelectionArticle : Locator;
+  private addToCartButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.addToCartButton = this.page.locator('#a-autoid-1-announce');
+    this.SelectionArticle = this.page.locator('.a-link-normal s-no-outline');
+    this.addToCartButton= this.page.locator("#add-to-cart-button")
   }
 
+  async gotoPageArticle(){
+    await this.SelectionArticle.click()
+  }
   async addToCart(){
     await this.addToCartButton.click()
   }
